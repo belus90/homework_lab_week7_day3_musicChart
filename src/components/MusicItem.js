24ec5e823b22b6775fe02784bb23ffb319305f2b
belus90 <belus90@gmail.com>
2022-04-07 09:15:00 +0100

    hw and lab work on extension

--- src/components/MusicItem.js
+++ src/components/MusicItem.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./MusicItems.css";
 
 
-const MusicItem = ({song}) => {
+const MusicItem = ({song, index}) => {
     return (
         <div>
-            <ul>
-            <p class = "p">{song.title.label}</p> 
+            <ul class = "p">
+                <p> Position # {index + 1}</p>
+            <p>{song["im:artist"].label}</p>
+            <p>{song.title.label}</p> 
+            <p>{song.id.label}</p>
+            <img src= {song["im:image"][0].label} alt="" />
             <br></br>
             </ul>
         </div>
